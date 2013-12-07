11f680e6c457dff904f383cf8800711ab51894ab
Thad Allender <thadallender@gmail.com>
2013-12-07 00:39:01 -0500

    site title position

--- js/onesie.js
+++ js/onesie.js
@@ -17,6 +17,17 @@ jQuery(document).ready(function($){
 		}
 	});
 
+	var distance = $('.site-title').offset().top;
+
+	$(window).scroll(function() {
+		
+		if ($(window).scrollTop() > distance) {
+			$('.site-title').addClass("fixed");
+		} else {
+			$('.site-title').removeClass("fixed");
+		}
+	});
+
 	$('.gallery-item').magnificPopup({
 		type: 'image',
 		gallery:{
